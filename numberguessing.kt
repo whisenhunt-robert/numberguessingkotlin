9d6f66a11505f5a7bc2098c4653b1bed6d41f5f6
whisenhunt-robert <dahweirdacus@gmail.com>
2023-03-19 01:06:29 -0400

    Added a few comments.

--- numberguessing.kt
+++ numberguessing.kt
@@ -6,14 +6,17 @@ fun main() {
     // This prints out the welcome message.
     println("Welcome to my Number Guessing Game using Kotlin!")
     while (true) {
+        // This prompts the player to enter a number between 0 and 100.
         print("Please enter a number between 0 and 100: ")
         // This store the player's guess as an integer.
         val playerGuess = Integer.parseInt(readLine())
         // If the player's guess is equal to the random number, this statement happens.
         if (playerGuess == randomNumber) {
+            // If the player guessed the randomly generated number, it prints this.
             println("That's correct! Great job!")
             return
         } else {
+            // If the player guessed incorrectly, it prints this.
             println("That's incorrect, please try again.")
         }
     }
